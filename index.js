54fbbb6656d2073c6b44056a08507d724663ea48
Muxsimov <muxsimovasadbek665@gmail.com>
2024-10-14 14:18:58 +0500

    finished

---
+++ index.js
@@ -0,0 +1,7 @@
+let math= +prompt("matematikadan olgan baholani kiriting");
+let physics= +prompt("fizikadan olgan baholani kiriting");
+let chemistry= +prompt("kimyodan olgan baholani kiriting");
+let biology= +prompt("biologiyadan olgan baholani kiriting");
+let rus=+prompt("rusdan olgan baholani kiriting");
+
+alert(`Siz kiritgan baholaringiz o'rta qiymati ${(math + physics + chemistry + biology + rus) / 5}`);
